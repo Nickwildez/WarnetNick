461a25b16b1e4e2483bed57350fd083e4800bd2e
Nickwildez <wildeznick@gmail.com>
2024-09-03 21:37:27 +0700

    update1

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function() {
+    const h2Page1 = document.querySelector('.h2-page-1');
+    h2Page1.style.animation = 'slideDown 1s forwards';
+
+    const pPage1 = document.querySelector('.p-page-1');
+    pPage1.style.animation = 'slideUp 1s forwards';
+
+    const btWarnetPage1 = document.getElementById('btWarnet-page-1');
+    btWarnetPage1.style.animation = 'slideUp 1s forwards';
+});
